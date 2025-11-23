b8d6c99d0872bcfce0f78245d38d9b4a24c4cd3b
Aneta Stala <a.stala.521@studms.ug.edu.pl>
2025-11-23 14:13:01 +0100

    Zad5

---
+++ Lab03/Zad5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()  
+{
+char znak;
+printf ("Podaj jeden znak \n");
+scanf ("%c", &znak);
+
+if (znak >= '0' && znak <= '9') 
+
+printf("Jest to liczba \n", znak);
+else if (znak >= 'A' && znak <= 'Z' || znak >= 'a' && znak <= 'z')
+printf ("Jest to litera \n", znak);
+else 
+printf ("Nie jest to litera ani liczba \n");
+}
+
+
+
+//    if (a > b && a > c)
+  //      printf("%d jest najwieksza", a); 
+
+  //  else 
+    //    printf("Liczby sa rowne\n"); 
+
+//char znak;
+//printf("Podaj znak");
+//scanf("%d", &znak);
+//printf("%c", znak);
+
+
