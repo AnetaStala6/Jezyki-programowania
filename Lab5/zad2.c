3d9b1a5432689c234dd8fa9d88dc29ecd9bcfcf7
Aneta Stala <a.stala.521@studms.ug.edu.pl>
2025-11-23 15:55:46 +0100

    dodanie zadania 2

---
+++ Lab5/zad2.c
@@ -0,0 +1,26 @@
+//Napisz program który wczyta od użytkownika parę liczb naturalnych a,b i zwróci wynik działania a do potęgi b. W rozwiązaniu nie kożystaj z potęgowania.
+
+#include<stdio.h>
+
+int main()  
+{
+int  a, b, wynik;
+printf("Podaj liczbe a i b: \n");
+scanf ("%d %d", &a, &b);
+
+if (a < 1) 
+printf("Podaj inna liczbe \n");
+
+else if (a >= 1)
+
+{
+for (wynik=1; b>0; --b)
+{
+    wynik *= a;
+}
+}
+
+printf("Wynik: %d \n", wynik);
+}
+
+
